8a3917231869e6880e6a3daedc2f54e77be122d6
Pablo Fernández Rodríguez <pafernan@redhat.com>
2026-02-14 15:55:51 +0100

    code cleanup
    Signed-off-by: Pablo Fernández Rodríguez <pafernan@redhat.com>

--- sosdiff/__init__.py
+++ sosdiff/__init__.py
@@ -12,37 +12,37 @@ class SosDiff:
         self.sos1 = False
         self.sos2 = False
 
-    def compare(self, s1, s2):
+    def compare_plugins(self, s1, s2):
         for plugin_name, plugin_data in s1.items():
             if plugin_name not in s2:
-                self.util.print_out(severity=4, message="missing plugin",
-                                    pluginname=plugin_name)
+                self.util.print_out(4, "-", plugin_name)
                 continue
-            for entity, data in plugin_data.items():
-                if entity not in s2[plugin_name]:
-                    self.util.print_out(severity=3, message="missing entity",
-                                        pluginname=plugin_name, entity=entity)
-                    continue
-                for k, v in data.items():
-                    if "href" in v and self.util.is_href_excluded(v["href"]):
-                        continue
-                    else:
-                        if k not in s2[plugin_name][entity]:
-                            self.util.print_out(
-                                severity=2, message="missing file",
-                                pluginname=plugin_name, entity=entity,
-                                file=k)
-                            continue
-                        if "href" in v:
-                            self.content_diff(plugin_name, entity, v["href"])
+            self.compare_entities(plugin_name, plugin_data, s2)
 
-    def content_diff(self, plugin_name, entity, file_path):
+    def compare_entities(self, plugin_name, plugin_data, s2):
+        for entity, data in plugin_data.items():
+            if entity not in s2[plugin_name]:
+                self.util.print_out(3, "-", plugin_name, entity=entity)
+                continue
+            self.compare_properties(plugin_name, entity, data, s2)
+
+    def compare_properties(self, plugin_name, entity, properties, s2):
+        for k, v in properties.items():
+            if "href" in v and self.util.is_href_excluded(v["href"]):
+                continue
+            if k not in s2[plugin_name][entity]:
+                self.util.print_out(2, "-", plugin_name,
+                                    entity=entity, file=k)
+                continue
+            if "href" in v:
+                self.compare_file_content(plugin_name, entity, v["href"])
+
+    def compare_file_content(self, plugin_name, entity, file_path):
         c1 = self.util.read_file(self.sospath1, file_path)
         c2 = self.util.read_file(self.sospath2, file_path)
         if c1 != c2:
-            self.util.print_out(severity=1, message="content diff",
-                                pluginname=plugin_name, entity=entity,
-                                file=file_path[2:])
+            self.util.print_out(1, "%", plugin_name,
+                                entity=entity, file=file_path[2:])
             if self.conf.args.diff:
                 self.util.print_diff(self.util.exec_command(
                     f"diff {self.sospath1}/{file_path[2:]} \
@@ -51,4 +51,4 @@ class SosDiff:
     def main(self):
         self.sos1 = self.util.load_sos_report(self.sospath1)
         self.sos2 = self.util.load_sos_report(self.sospath2)
-        self.compare(self.sos1, self.sos2)
+        self.compare_plugins(self.sos1, self.sos2)

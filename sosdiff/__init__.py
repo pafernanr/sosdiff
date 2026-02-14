f9e757376db7fe878fd4768be79642071ae1aafa
Pablo Fernández Rodríguez <pafernan@redhat.com>
2026-02-14 09:16:06 +0100

    output format improvements and some code cleanup
    Signed-off-by: Pablo Fernández Rodríguez <pafernan@redhat.com>

--- sosdiff/__init__.py
+++ sosdiff/__init__.py
@@ -1,5 +1,5 @@
-import re
 import json
+import re
 
 from sosdiff.lib.configuration import Conf
 from sosdiff.lib.util import Util
@@ -20,15 +20,6 @@ class SosDiff:
             r'^\.\./var/log/.*$',
             r'^\.\./sos_strings/.*var\.log.*\.tailed$']
 
-    def load_sos_report(self, sosreport_path):
-        try:
-            return self.get_plugins(json.loads(
-                self.read_file(sosreport_path, "../sos_reports/sos.json")
-                ))
-        except Exception as e:
-            print(e)
-        return False
-
     def is_plugin_included(self, plugin_name):
         if len(self.included_plugins) > 0:
             if plugin_name not in self.included_plugins:
@@ -45,7 +36,7 @@ class SosDiff:
         return False
 
     def get_plugins(self, sos):
-        sos = self.transform_plugin_list_to_dict(sos)
+        sos = self.util.transform_plugin_list_to_dict(sos)
         out = dict(sos)
         for plugin_name, plugin_data in sos.items():
             if not self.is_plugin_included(plugin_name):
@@ -53,61 +44,48 @@ class SosDiff:
                 continue
             for name, data in plugin_data.items():
                 if name in ["commands", "copied_files", "created_files"]:
-                    out[plugin_name][name] = self.transform_list_to_dict(data)
+                    out[plugin_name][name] = self.util.transform_list_to_dict(
+                        data)
                 else:
                     print(f"ERR: {name}")
         return out
 
-    def transform_plugin_list_to_dict(self, items):
-        out = {}
-        for i in items:
-            out[i[0]] = i[1]
-        return out
-
-    def transform_list_to_dict(self, items):
-        out = {}
-        for c in items:
-            name = c['name']
-            c.pop('name')
-            out[name] = c
-        return out
+    def load_sos_report(self, sosreport_path):
+        try:
+            return self.get_plugins(json.loads(
+                self.util.read_file(sosreport_path, "../sos_reports/sos.json")
+                ))
+        except Exception as e:
+            print(e)
+        return False
 
     def compare(self, s1, s2):
         for plugin_name, plugin_data in s1.items():
             if plugin_name not in s2:
-                print(f"[MISS_P] {plugin_name}")
+                self.util.show_diff(1, "plugin missing", plugin_name)
                 continue
             for name, data in plugin_data.items():
                 if name not in s2[plugin_name]:
-                    print(f"[MISS_N] {plugin_name} :: {name}")
+                    self.util.show_diff(1, "name missing",
+                                        f"{plugin_name} :: {name}")
                     continue
                 for k, v in data.items():
                     if "href" in v and self.is_href_excluded(v["href"]):
                         continue
                     else:
                         if k not in s2[plugin_name][name]:
-                            print(f"[MISS_I] {plugin_name} :: {name} :: {k}")
+                            self.util.show_diff(1, "file missing",
+                                           f"{plugin_name} :: {name} :: {k}")
                             continue
                         if "href" in v:
                             self.contentdiff(plugin_name, name, v["href"])
 
     def contentdiff(self, plugin_name, name, file_path):
-        c1 = self.read_file(self.sospath1, file_path)
-        c2 = self.read_file(self.sospath2, file_path)
+        c1 = self.util.read_file(self.sospath1, file_path)
+        c2 = self.util.read_file(self.sospath2, file_path)
         if c1 != c2:
-            print(f"[DIFFER] {plugin_name} :: {name} :: {file_path[2:]}")
-
-    def read_file(self, sospath, file_path):
-        abs_path = f"{sospath}/{file_path[2:]}"
-        try:
-            if self.util.file_is_binary(abs_path):
-                with open(f"{abs_path}", "rb") as f:
-                    return f.read()
-            else:
-                with open(f"{abs_path}", "r", encoding="utf-8") as f:
-                    return f.read()
-        except Exception as e:
-            print(f"Error reading file {abs_path}\n{e}")
+            self.util.show_diff(2, "file content",
+                                f"{plugin_name} :: {name} :: {file_path[2:]}")
 
     def main(self):
         self.sos1 = self.load_sos_report(self.sospath1)
--- sosdiff/lib/util.py
+++ sosdiff/lib/util.py
@@ -1,7 +1,3 @@
-import datetime
-import subprocess
-import sys
-
 magic_mod = False
 try:
     import magic
@@ -13,8 +9,41 @@ except (ImportError, AttributeError):
 
 class Util:
 
+    def show_diff(self, level, name, text):
+        if level > 1:
+            color = Style.CYAN_BOLD
+        else:
+            color = Style.YELLOW_BOLD
+        print(f"{color}[{name}]{Style.RESET} {text}")
+
+    def read_file(self, sospath, file_path):
+        abs_path = f"{sospath}/{file_path[2:]}"
+        try:
+            if self.file_is_binary(abs_path):
+                with open(f"{abs_path}", "rb") as f:
+                    return f.read()
+            else:
+                with open(f"{abs_path}", "r", encoding="utf-8") as f:
+                    return f.read()
+        except Exception as e:
+            print(f"Error reading file {abs_path}\n{e}")
+
+    def transform_plugin_list_to_dict(self, items):
+        out = {}
+        for i in items:
+            out[i[0]] = i[1]
+        return out
+
+    def transform_list_to_dict(self, items):
+        out = {}
+        for c in items:
+            name = c['name']
+            c.pop('name')
+            out[name] = c
+        return out
+
     def file_is_binary(self, fname):
-        ''' Method from https://github.com/sosreport/sos/blob/main/sos/utilities.py '''
+        ''' Method from https://github.com/sosreport/sos/blob/main/sos/utilities.py '''  # noqa E501
         if magic_mod:
             try:
                 _ftup = magic.detect_from_filename(fname)
@@ -25,8 +54,9 @@ class Util:
                 )
             except Exception:
                 pass
-        # if for some reason the above check fails or magic>=0.4.20 is not present,
-        # fail over to checking the very first byte of the file content
+        # if for some reason the above check fails or magic>=0.4.20 is not
+        # present, fail over to checking the very first byte of the file
+        # content
         with open(fname, 'tr', encoding='utf-8') as tfile:
             try:
                 # when opened as above (tr), reading binary content will raise
@@ -35,3 +65,22 @@ class Util:
                 return False
             except UnicodeDecodeError:
                 return True
+
+
+class Style:
+    GREEN = '\033[0;32m'
+    GREEN_BOLD = '\033[0;32m\033[1m'
+    RED = '\033[0;31m'
+    RED_BOLD = '\033[0;31m'
+    YELLOW = '\033[01;33m'
+    YELLOW_BOLD = '\033[01;33m\033[1m'
+    BLUE = '\033[0;34m'
+    BLUE_BOLD = '\033[0;34m\033[1m'
+    CYAN = '\033[01;36m'
+    CYAN_BOLD = '\033[01;36m\033[1m'
+    PURPLE = '\033[0;35m'
+    PURPLE_BOLD = '\033[0;35m\033[1m'
+    BOLD = '\033[1m'
+    RESET = '\033[0m'
+    NEW_LINE = '\n'
+    RESET_NEW_LINE = '\033[0m\n'
